ca96e5ac2f3c7df40d9dd3ee95d710def4bb5ba8
Usman Akram <usman.akram@gmail.com>
2021-04-09 15:03:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.get("/", function (req, res) {
+  return res.send("Hello B Section");
+});
 app.get("/api/customers", function (req, res) {
   return res.send(["Noman", "Ali", "Kamran"]);
 });
