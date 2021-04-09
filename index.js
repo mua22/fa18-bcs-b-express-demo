dc15af1d8a12d900e06f25ac4ec3218eff14bc75
Usman Akram <usman.akram@gmail.com>
2021-04-09 15:22:42 +0500

    pug integrated

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.set("view engine", "pug");
 
-app.get("/", function (req, res) {
-  return res.send("Hello B Section");
+app.get("/hobbies", function (req, res) {
+  return res.render("hobbies");
 });
 app.get("/api/customers", function (req, res) {
   return res.send(["Noman", "Ali", "Kamran"]);
 });
-
+app.get("/", function (req, res) {
+  return res.render("homepage");
+});
 app.listen(process.env.PORT || 4000, function () {
   console.log(`Listening on Port 4000....`);
 });
